fedde4d546b3495fb58c69d8fcbdf309bfd85dfc
kevinalappat09 <35099148+kevinalappat09@users.noreply.github.com>
2023-12-15 14:42:29 +0530

    Added form validation

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 const submitButton = document.querySelector(".submit-button");
 
 const authorInput = document.querySelector("#author-input");
+authorInput.addEventListener("input",(event)=> {
+    if(authorInput.validity.valid) {
+        authorInput.parentNode.querySelector(".error").textContent = "";
+        authorInput.parentNode.querySelector(".error").className = "error";
+    } else {
+        if(authorInput.validity.valueMissing) {
+            authorInput.parentNode.querySelector(".error").textContent = "You need to enter an author";
+        }
+
+        authorInput.parentNode.querySelector(".error").className = "error active";
+    }
+})
 const titleInput = document.querySelector("#title-input");
+titleInput.addEventListener("input",(event)=> {
+    if(titleInput.validity.valid) {
+        titleInput.parentNode.querySelector(".error").textContent = "";
+        titleInput.parentNode.querySelector(".error").className = "error";
+    } else {
+        if(titleInput.validity.valueMissing) {
+            titleInput.parentNode.querySelector(".error").textContent = "You need to have a title";
+        }
+
+        titleInput.parentNode.querySelector(".error").className = "error active";
+    }
+})
 const pageInput = document.querySelector("#pages-input");
+pageInput.addEventListener("input",(event) => {
+    if(pageInput.validity.valid) {
+        pageInput.parentNode.querySelector(".error").textContent = "";
+        pageInput.parentNode.querySelector(".error").className = "error";
+     } else {
+        if(pageInput.validity.valueMissing) {
+            pageInput.parentNode.querySelector(".error").textContent = "Input page numbers";
+        } else if(pageInput.validity.typeMismatch) {
+            pageInput.parentNode.querySelector(".error").textContent = "Page must be a number";
+        }
+        pageInput.parentNode.querySelector(".error").className = "error active"
+     }
+})
 const is_readInput = document.querySelector('#is_read-input');
 
 const libraryElement = document.querySelector('.library');
@@ -23,6 +60,8 @@ class book {
 }
 
 
+
+
 function add_book(title, author, pages, is_read) {
     const new_book = new book(title,author,pages,is_read);
     myLibrary.push(new_book);
@@ -33,6 +72,8 @@ function addLibraryBook() {
     titleInputValue = titleInput.value;
     pageInputValue = pageInput.value;
     is_readInputValue = is_readInput.checked;
+
+
 
     if(is_readInputValue==false) {
         add_book(titleInputValue,authorInputValue,pageInputValue,false);
@@ -53,7 +94,7 @@ function delDiv(event) {
 }
 
 function isreadToggle(event) {
-    target = event.target.parentElement;
+    target = event.target.parentElement.parentElement;
 
     if(myLibrary[target.getAttribute('data-id')].is_read === true) {
         myLibrary[target.getAttribute('data-id')].is_read = false;
